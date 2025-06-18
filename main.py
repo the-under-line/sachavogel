cdb08f61187172ee72f5e29e2c3fabc0d484819a
The UnderLine <114292928+the-under-line@users.noreply.github.com>
2025-06-18 09:10:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from js import document
 
 def update_file():
+    output = document.getElementById("output")
+    
 
     with open("visitors.txt") as file:
         data = int(file.read())
     with open("visitors.txt", "w") as file:
         file.write(str(data+1))
+    output.innerHTML = f"Current Count: {data+1}"
