255ec3b702b1dbcf7fe42b15b67515ef3f301c66
The UnderLine <114292928+the-under-line@users.noreply.github.com>
2025-06-18 09:14:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def update_file(*args, **kwargs):
     except FileNotFoundError:
         with open("visitors.txt", "w") as f:
             f.write("0")
-            data = "0"
+            data = 0
     with open("/visitors.txt", "w") as file:
         file.write(str(data+1))
     output.innerHTML = f"Current Count: {data+1}"
